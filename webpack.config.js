17eaeac9cbe0f498740dbceb5a4a72f68313dca5
good-singer <15732658216@163.com>
2019-07-12 10:26:24 +0800

    添加 webpack 的 externals 配置

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: {
     index: './lib/index.tsx'
   },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
   output: {
     path: path.resolve(__dirname, 'dist/lib'),
     library: 'Yui',
@@ -23,5 +26,19 @@ module.exports = {
       title: 'Yui',
       template: 'index.html'
     })
-  ]
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    }
+  },
 }
